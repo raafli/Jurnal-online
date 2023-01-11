a67a82d429ef5bca07de769ad0ad94cf94f91bb0
raafli <muhnarendra66@gmail.com>
2023-01-11 15:54:49 +0700

    a

---
+++ app/Models/mapping.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class mapping extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/smonitoring.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class smonitoring extends Model
+{
+    use HasFactory;
+}
